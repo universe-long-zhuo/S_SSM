6212addada4309d281715c3595d5441180b1c872
longzhuo <1009433228@qq.com>
2016-09-21 14:11:36 +0800

    0.0
    系统监控SSM完整源码和基本的Springmvcfirst1001源码

---
+++ springmvcfirst1001/src/com/app/po/Items.java
@@ -0,0 +1,61 @@
+package com.app.po;
+
+import java.util.Date;
+
+public class Items {
+	
+    private Integer id;       // 商品ID
+    private String name;      // 商品姓名
+    private Float price;      // 商品价格
+    private String pic;       // 商品图片
+    private Date createtime;  // 商品时间
+    private String detail;    // 商品明细
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public String getPic() {
+        return pic;
+    }
+
+    public void setPic(String pic) {
+        this.pic = pic == null ? null : pic.trim();
+    }
+
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail == null ? null : detail.trim();
+    }
+}
